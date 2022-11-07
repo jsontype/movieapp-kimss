608389c2a698cefb84c73780b86337c18ffba754
jsontype <jsontyper@gmail.com>
2022-11-07 20:29:01 +0900

    sadf

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,27 +1,24 @@
 import React from 'react'
 
 export default function MovieList({movies}) {
+  const render = movies.map(item => {
+    //JS
+    const movieRankClass = item.rating >= 8 ? 'good' :item.rating >=6 ? 'soso' : 'bad'
+    const hotIcon = item.rating >= 9 && '🔥'
 
-    const render = movies.map(item => {
-        //JS
-          const movieRankClass = item.rating >= 8 ? 'good' :item.rating >=6 ? 'soso' : 'bad'
-          const hotIcon = item.rating >= 9 && '🔥'
+    //XML  
+    return (
+      <div key={item.id}>
+        <a className='movieTitle' href={item.url}>{hotIcon}{item.title}</a>
+        <div>평점 : <span className={movieRankClass}>{item.rating}</span> /10점</div>
+        <img src={item.large_cover_image} alt={item.title}></img>
+      </div>
+    )
+  })
 
-        
-        //XML  
-          return (
-            <div key={item.id}>
-              <a className='movieTitle' href={item.url}>{hotIcon}{item.title}</a>
-              <div>평점 : <span className={movieRankClass}>{item.rating}</span> /10점</div>
-              <img src={item.large_cover_image} alt={item.title}></img>
-            </div>
-          )
-        })
   return (
     <>
-    {render}
+      {render}
     </>
   )
 }
-
-
